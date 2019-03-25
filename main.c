381ef8348d179f5d9161a57811cf8a2ea20bf1c5
timowka0304 <timoti20@mail.ru>
2019-03-25 14:33:38 +0700

    Edit

--- main.c
+++ main.c
@@ -30,9 +30,9 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, N_ELEMENTS(arr));
+    const int min = min_element(arr, 7);
 
-    print_array(arr, N_ELEMENTS(arr));
+    print_array(arr, 7);
     printf("Min element: %d\n", min);
 
     return 0;
